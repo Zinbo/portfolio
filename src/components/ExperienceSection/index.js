7de4b8260d35804eed6dc5f86d26d60cdbd2f47a
Shane <shanepjennings@gmail.com>
2020-04-10 18:30:16 +0100

    remove tempalte projects and experience

--- src/components/ExperienceSection/index.js
+++ src/components/ExperienceSection/index.js
@@ -11,34 +11,6 @@ class ExperienceSection extends React.Component {
     return (
       <Section title="Experience">
         <div className="row">
-          <ExperienceUnit
-            logo={zomatoLogo}
-            colour='#CB202D'
-            title='Zomato'
-            link='https://www.zomato.com/'
-            timeperiod='2014 - 2015'
-            subtitle='Learnt the ins and outs of product development and
-              built various features including a completely new product.'
-          />
-          <ExperienceUnit
-            logo={fitsoLogo}
-            colour='#FFFFFF'
-            title='Fitso'
-            link='https://www.getfitso.com/'
-            timeperiod='2016'
-            subtitle='Built real time GPS tracking on the iOS app,
-              all consumer facing web modules and external APIs in
-              this early stage startup.'
-          />
-          <ExperienceUnit
-            logo={kayakoLogo}
-            colour='#403949'
-            title='Kayako'
-            link='https://www.kayako.com/'
-            timeperiod='2017 - 2018'
-            subtitle='Worked as a Product Engineer on email processing,
-              push notifications, SLAs amongst other things.'
-          />
         </div>
       </Section>
     )
